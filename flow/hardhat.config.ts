b3bfbc3c38725968e517c961f1672acc3e8350ee
Palash A <palash.agarwal.96@gmail.com>
2025-06-23 18:55:07 -0400

    update hardhat config

--- flow/hardhat.config.ts
+++ flow/hardhat.config.ts
@@ -1,8 +1,41 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    flowTestnet: {
+      url: 'https://testnet.evm.nodes.onflow.org',
+      accounts: [process.env.DEPLOY_WALLET_1 as string],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      // Is not required by blockscout. Can be any non-empty string
+      'flow': "abc",
+      'flowTestnet': "abc"
+    }, 
+    customChains: [
+      {
+        network: 'flow',
+        chainId: 747,
+        urls: {
+          apiURL: 'https://evm.flowscan.io/api',
+          browserURL: 'https://evm.flowscan.io/',
+        },
+      },
+      {
+        network: 'flowTestnet',
+        chainId: 545,
+        urls: {
+          apiURL: 'https://evm-testnet.flowscan.io/api',
+          browserURL: 'https://evm-testnet.flowscan.io/',
+        },
+      },
+    ],
+  },
 };
 
 export default config;
